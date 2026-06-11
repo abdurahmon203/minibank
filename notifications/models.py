24f59a79cce89048db659f9d9870c3e5f2daa50a
Abdurahmon <nozimovislom101@gmail.com>
2026-06-11 22:55:19 +0500

    Added Notifications Model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Notification(models.Model):
+
+    class NotificationType(models.TextChoices):
+        TRANSACTION = "TRANSACTION", "TRANSACTION"
+        PAYMENT = "PAYMENT", "PAYMENT"
+        SYSTEM = "SYSTEM", "SYSTEM"
+        CARD = "CARD", "CARD"
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="notifications"
+    )
+
+    title = models.CharField(max_length=255)
+
+    message = models.TextField()
+
+    notification_type = models.CharField(
+        max_length=20,
+        choices=NotificationType.choices
+    )
+
+    is_read = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
